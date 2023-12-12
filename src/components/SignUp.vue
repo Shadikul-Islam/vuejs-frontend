b2927e7cf5bc67dd6eeeceb95ec0e552998ef608
Shadikul Islam <shadikul.islam@datasoft-bd.com>
2023-12-12 17:17:28 +0600

    Signup Page for Students

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,12 +1,13 @@
 <template>
     <h2>Student Signup Page</h2>
     <div class="signup">
-        <input type="text" placeholder="Enter Full Name"> <br>
-        <input type="text" placeholder="Enter Email"> <br>
-        <input type="text" placeholder="Enter ID"> <br>
-        <input type="password" placeholder="Enter Password"> <br>
-        <input type="password" placeholder="Confirm Password"> <br>
+        <form @submit.prevent="submitForm"> 
+        <input type="text" v-model="fullname" placeholder="Enter Full Name" > <br>
+        <input type="text" v-model="email" placeholder="Enter Email"> <br>
+        <input type="text" v-model="studentId" placeholder="Enter Student ID"> <br>
+        <input type="password" v-model="password" placeholder="Enter Password"> <br>
         <button type="submit">Submit</button>
+    </form>
     </div>
 </template>
 
--- src/components/js/SignUp.js
+++ src/components/js/SignUp.js
@@ -1,3 +1,31 @@
+import axios from 'axios'
 export default{
-    name: "SignUp"
+    name: "SignUp",
+
+    data() {
+        return {
+            fullname:"",
+            email:"",
+            studentId:"",
+            password:""
+        }
+    },
+    methods: {
+        submitForm() {
+            let params = new FormData();
+            params.append('fullname', this.fullname);
+            params.append('email', this.email);
+            params.append('studentId', this.studentId);
+            params.append('password', this.password);
+            axios.post('http://192.168.26.145/shadikul/backend/index.php/api/students/index_post', params).then(response => {
+            this.success = 'Data saved successfully';
+            alert(this.success);
+          }).catch(error => {
+            this.response = 'Error: ' + error.response.status
+            console.log(this.response);
+          })
+        }
+      },
+   
+
 }
